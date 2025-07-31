9184087f6ddf70a9c26e843bcb53a25e03d1758e
naoto24kawa <naoto24kawa@gmail.com>
2025-07-31 16:04:56 +0900

    feat: Add GitHub Actions workflow for deployment to GitHub Pages and fix project name in package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/sweep-game/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
